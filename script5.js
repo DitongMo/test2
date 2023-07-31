2384579313c1a45a21d6066b72c1614e768cc2b9
DitongMo <77713981+DitongMo@users.noreply.github.com>
2023-07-31 15:55:56 -0400

    Update script5.js

--- script5.js
+++ script5.js
@@ -1,14 +1,3 @@
-var margin = {top: 30, right: 30, bottom: 70, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
-var svg = d3.select("#chart5")
-    .append("svg")
-    .attr("width", "100%")
-    .attr("height", "100%")
-    .attr("viewBox", "0 0 800 400")
-    .append("g")
-    .attr("transform", "translate(50, 50)");
 
 // Load the data from CSV file
 d3.csv("cases_by_age_group_v3.csv").then(function(data) {
@@ -22,6 +11,17 @@ d3.csv("cases_by_age_group_v3.csv").then(function(data) {
   // });
 
     // set the dimensions and margins of the graph
+var margin = {top: 30, right: 30, bottom: 70, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+var svg = d3.select("#chart5")
+    .append("svg")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr("viewBox", "0 0 800 400")
+    .append("g")
+    .attr("transform", "translate(50, 50)");
 
   
 // X axis
